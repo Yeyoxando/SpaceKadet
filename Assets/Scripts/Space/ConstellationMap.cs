1644edc08c04f886e101d2faa73c7ec754b6c3d6
Diego Ochando Torres <yeyoxando@gmail.com>
2024-08-21 15:07:20 +0200

    Initial commit. Constellation and spaceship systems scripts

---
+++ Assets/Scripts/Space/ConstellationMap.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConstellationMap : MonoBehaviour{
+
+  Orion nightSkyHunter;
+
+  void Start(){
+        
+  }
+  
+  void Update(){
+        
+    }
+}
---
+++ Assets/Scripts/Space/Orion.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+// The Night Sky Hunter
+public class Orion : MonoBehaviour {    
+    void Start(){
+        
+    }
+
+    void Update(){
+        
+    }
+}
+
+/*
+BRIGHT STARS:
+- Betelgeuse (Alpha Orionis) (M-type red supergiant)
+- Rigel (Beta Orionis) (B-type blue supergiant)
+- Bellatrix (Gamma Orionis) (B-type blue giant)
+- Mintaka (Delta Orionis) (Multiple star system, 
+    Btype blue giant, O-type main-sequence star, 
+    eclipsing binary variable star)
+- Alnilam (Epsilon Orionis) (B-type blue supergiant)
+- Alnitak (Zeta Orionis) (hot blue supergiant, brightest O star)
+- Saiph (Kappa Orionis) (similar to Rigel, sword of the giant)
+- Meissa (Lambda Orionis) (Orion's head, multiple star, 
+    means the shining one)
+ */
